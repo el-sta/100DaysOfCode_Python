16bf3b06309abc3bd85491c6b7ab23adbe756e04
el-sta <162905452+el-sta@users.noreply.github.com>
2024-09-19 20:51:00 +0300

    Add files via upload

---
+++ #Day1 project-Band name generator.py
@@ -0,0 +1,14 @@
+#Day1 project-Band name generator
+
+#print a welcome message
+print("Welcome to the Band Name Generator!")
+
+#ask the users what's the name of city they grew up in
+city= input("What's the name of the city you grew up in?\n")
+
+
+#ask the users what's the name of their favourite pet
+pet= input("What's the name of your favourite pet?\n")
+
+#print the possible band name
+print("The band name could be " + city + " " + pet + "!")
